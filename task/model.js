b70fb082ecf16d333e7b91ecfcc7cbaeec7a405c
Theodor Johansson <t.j.u@telia.com>
2024-09-27 13:45:03 +0200

    changed sqlite to use Sequalize

--- task/model.js
+++ task/model.js
@@ -1,76 +1,35 @@
-import sqlite from "sqlite3"
+import {Sequalize} from "sequalize"
 
-const db = new sqlite.Database("./task.db")
+const sequalize = new Sequalize({
+    dialect: 'sqlite',
+    storage: './task.db',
+})
 
+const Tasks = sequalize.define(
+    'Tasks',
+    {
+        name: {
+            type: Sequalize.STRING,
+        },
+        time: {
+            type: Sequalize.INTEGER,
+        }
+    },
+    {timestamps: false},
+)
 
-export async function getAll() {
-    return new Promise((resolve, reject) => {
-        const query = 'SELECT * FROM tasks';
-        db.all(query, (error, results) => {
-            if (error) {
-                reject(error)
-            } else {
-                resolve(results)
-            }
-        })
-    })
+export function getAll() {
+    return Tasks.findAll()
 }
 
-function insert(task) {
-    return new Promise((resolve, reject) => {
-        const query = 'INSERT INTO Tasks (name, time) VALUES (?, ?)'
-        db.run(query, [task.name, task.time], (error, results) => {
-            if (error) {
-                reject(error)
-            } else {
-                resolve(results)
-            }
-        })
-    })
+export function get(id) {
+    return Tasks.findByPk(id)
 }
 
-function update(task) {
-    return new Promise((resolve, reject) => {
-        const query = 'UPDATE Tasks SET name = ?, time = ? WHERE id = ?'
-        db.run(query, [task.name, task.time, task.id], (error, results) => {
-            if (error) {
-                reject(error)
-            } else {
-                resolve(results)
-            }
-        })
-    })
+export function remove(id) {
+    return Tasks.destroy({where: {id}})
 }
 
-export async function get(id) {
-    return new Promise((resolve, reject) => {
-        const query = 'SELECT * FROM Tasks WHERE id = ?'
-        db.get(query, [id], (error, results) => {
-            if (error) {
-                reject(error)
-            } else {
-                resolve(results)
-            }
-        })
-    })
-}
-
-export async function save(task) {
-    if (!task.id) {
-        return insert(task)
-    } else {
-        return update(task)
-    }
-}
-export async function remove(id) {
-    return new Promise((resolve, reject) => {
-        const query = 'DELETE FROM Tasks WHERE id = ?'
-        db.run(query, [id], (error, results) => {
-            if (error) {
-                reject(error)
-            } else {
-                resolve(results)
-            }
-        })
-    })
+export function save(task) {
+    return Tasks.upsert(task)
 }
